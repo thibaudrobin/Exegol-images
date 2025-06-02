9590653c589e40c80328e28513801221d5e4e92a
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2025-06-02 10:14:12 +0200

    snoozing temp fixes

--- sources/install/package_base.sh
+++ sources/install/package_base.sh
@@ -50,7 +50,7 @@ function install_go() {
     #asdf install golang latest
     #asdf set --home golang latest
     # With golang 1.23 many package build are broken, temp fix to use 1.22.2 as golang latest
-    local temp_fix_limit="2025-06-01"
+    local temp_fix_limit="2025-07-01"
     if check_temp_fix_expiry "$temp_fix_limit"; then
       # 1.23 needed by BloodHound-CE, and sensepost/ruler
       asdf install golang 1.23.0
